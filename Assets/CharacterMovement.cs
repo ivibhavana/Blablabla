7da771be87dced5da158849da06d316574ff6ba9
ivibhavana <ivanaaliaga23@gmail.com>
2023-11-10 20:00:21 -0300

    Avance_tarea_1.-11-1992

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public float jumpSpeed = 2.0f;
+    public float gravity = 10.0f;
+    private Vector3 jumpForce = Vector3.zero;
+
+    public float coyoteTime = 0.2f;
+    private float coyoteCurrent = 0;
+
+    [Header("Movimiento")]
+    public CharacterController controller;
+    public Transform cam;
+
+    [Header("Animaciones")]
+    public Animator animator;
+
+    private void Start()
+    {
+        // Hide cursor
+        Cursor.visible = false;
+    }
+
+    public void Update()
+    {
+        // Get input from keyboard
+        var H_axis = Input.GetAxis("Horizontal");
+        var V_axis = Input.GetAxis("Vertical");
+        var dir = new Vector3(H_axis, 0, V_axis).normalized;
+
+        // get jump input
+        var jump = Input.GetButtonDown("Jump");
+        if ((controller.isGrounded || coyoteCurrent <= coyoteTime) && jump)
+        {
+            // Set jump force
+            jumpForce.y = jumpSpeed;
+
+            // Set animator jump trigger
+            animator.SetTrigger("Jumping");
+        }
+
+        // Set animator grounded parameter
+        animator.SetBool("Grounded", controller.isGrounded);
+
+        coyoteCurrent = 0;
+
+        // Apply gravity
+        if (!controller.isGrounded) // si no estoy en el suelo
+        {
+            coyoteCurrent += Time.deltaTime;
+
+            jumpForce.y -= gravity * Time.deltaTime;
+        }
+        controller.Move(jumpForce * Time.deltaTime);
+
+        if (dir.magnitude >= 0.1f)
+        {
+            // Set animator running parameter to true
+            animator.SetBool("Running", true);
+
+            // Rotate the character to the direction of movement
+            var targetAngle = (Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg) + cam.eulerAngles.y;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Move the character
+            var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir * speed * Time.deltaTime);
+        }
+        else
+        {
+            // Set animator running parameter to false
+            animator.SetBool("Running", false);
+        }
+    }
+}
--- Assets/Movimiento.cs
+++ Assets/Movimiento.cs
@@ -10,49 +10,82 @@ public class Movimiento : MonoBehaviour
     public float speed = 5f;
     public float jumpSpeed = 5f;
 
+    private Vector3 moveDirection = Vector3.zero; // Usaremos esto para el movimiento total
     private Vector3 jumpForce = Vector3.zero;
     public float gravity = 10f;
     public Camera cam;
-    
+
+    // Coyote time
+    public float coyoteTime = 0.2f;
+    private float coyoteCurrent;
+
+    // Doble salto
+    private int jumpsRemaining;
+    public int maxJumps = 2;
+
     void Start()
     {
-        
+        coyoteCurrent = coyoteTime;
+        jumpsRemaining = maxJumps;
     }
 
     void Update()
     {
-        //Get axis horizontal y vertical
+        // Obtener ejes horizontal y vertical
         float H_axis = Input.GetAxis("Horizontal");
         float V_axis = Input.GetAxis("Vertical");
 
-        //Debug.Log("H: * + H_axis + * V:* + V_axis);
+        // Vector de dirección normalizado
+        Vector3 dir = new Vector3(H_axis, 0, V_axis).normalized;
 
-        //Vector3 dir = new Vector3(H_axis, 0 , V_axis);
-        Vector3 dir = new Vector3(H_axis, 0 , V_axis).normalized;
-        //Debug.Log(dir);
-
-        if(dir.magnitude >= 0.1f)
-
-        float angle = (Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg)
-        float angle2 = Mathf SmoothDeepAngle(transform.eulerAngles.y, angl, ref turnSmoothVel, turnSmoothTime)
+        if (dir.magnitude >= 0.1f)
         {
-            //Debug.Log(dir.magnitude);
-            var dir2 = Quaternium.Euler(0f, angle, 0f);
-            transform rotation = quaternion.Euler(0f, angle, 0f)
-            CharacterController.Move(dir * speed * Time.deltaTime);
+            // Calcular el ángulo basado en la dirección y la rotación de la cámara
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+
+            // Calcular el ángulo suavizado
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+
+            // Actualizar la rotación del jugador
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Establecer la dirección de movimiento total
+            moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            // Mover al personaje en la dirección calculada
+            CharacterController.Move(moveDirection * speed * Time.deltaTime);
         }
+
+        // Aplicar gravedad
         jumpForce.y -= gravity * Time.deltaTime;
 
+        // Verificar si se presiona el botón de salto
         bool jumpBtn = Input.GetButtonDown("Jump");
 
-       if(jumpBtn)
-       {
-        jumpForce.y = jumpSpeed * Time.deltaTime;
-       
-       }
+        if (CharacterController.isGrounded)
+        {
+            // Restablecer los saltos restantes si el personaje está en el suelo
+            jumpsRemaining = maxJumps;
+            coyoteCurrent = 0; // Reiniciar el coyote time
+        }
 
+        // Verificar el doble salto
+        if (jumpBtn && (CharacterController.isGrounded || coyoteCurrent > 0) && jumpsRemaining > 0)
+        {
+            // Aplicar fuerza de salto si el personaje está en el suelo o en el coyote time y tiene saltos restantes
+            jumpForce.y = jumpSpeed;
+
+            // Reducir los saltos restantes
+            jumpsRemaining--;
+
+            // Restablecer el coyote time después de un salto en el aire
+            coyoteCurrent = 0;
+        }
+
+        // Mover al personaje en la dirección vertical (saltar)
         CharacterController.Move(jumpForce * Time.deltaTime);
 
-
+        // Actualizar el coyote time
+        coyoteCurrent += Time.deltaTime;
     }
 }
